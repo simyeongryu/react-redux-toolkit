2c86b8d9eb85fd0d2e3098c217d5e6eeb894e3cc
simyeongryu <rsm0503@gmail.com>
2020-08-08 20:56:21 +0900

    done

--- src/store.js
+++ src/store.js
@@ -1,30 +1,48 @@
 import { createStore } from 'redux';
-import { createAction, createReducer, configureStore } from '@reduxjs/toolkit';
+import {
+  createAction,
+  createReducer,
+  configureStore,
+  createSlice
+} from '@reduxjs/toolkit';
 
-// const action = create(type)
-const addToDo = createAction('ADD');
-const deleteToDo = createAction('DELETE');
+// // const action = create(type)
+// const addToDo = createAction('ADD');
+// const deleteToDo = createAction('DELETE');
 
-// createReducer(초기 state)
-const reducer = createReducer([], {
-  [addToDo]: (state, action) => {
-    state.push({ text: action.payload, id: Date.now() });
-  },
+// // createReducer(초기 state)
+// const reducer = createReducer([], {
+//   [addToDo]: (state, action) => {
+//     state.push({ text: action.payload, id: Date.now() });
+//   },
 
-  [deleteToDo]: (state, action) => {
-    return state.filter(toDo => toDo.id !== action.payload);
+//   [deleteToDo]: (state, action) => {
+//     return state.filter(toDo => toDo.id !== action.payload);
+//   }
+// });
+
+const toDos = createSlice({
+  name: 'toDosReducer',
+  initialState: [],
+  reducers: {
+    addToDo: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    },
+    deleteToDo: (state, action) => {
+      return state.filter(toDo => toDo.id !== action.payload);
+    }
   }
 });
 
-// const store = createStore(reducer);
+// reducer에 toDos의 reducers 할당
+const store = configureStore({ reducer: toDos.reducers });
+console.log(toDos.actions);
 
-// middleware를 사용하면서 store를 생성한다.
-// 일례로 Redux Dev Tools를 사용할 수 있다.
-const store = configureStore({ reducer });
+// export const actionCreators = {
+//   addToDo,
+//   deleteToDo
+// };
 
-export const actionCreators = {
-  addToDo,
-  deleteToDo
-};
+export const { addToDo, deleteToDo } = toDos.actions;
 
 export default store;
