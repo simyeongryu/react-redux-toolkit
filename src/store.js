636b343c5ab281ad579bb3859c2f6e0600f4b62d
simyeongryu <rsm0503@gmail.com>
2020-08-08 20:06:19 +0900

    createAction

--- src/store.js
+++ src/store.js
@@ -1,46 +1,10 @@
 import { createStore } from 'redux';
 import { createAction } from '@reduxjs/toolkit';
 
-/** *
-// 일반 리덕스
-const ADD = 'ADD';
-const DELETE = 'DELETE';
-
-const addToDo = text => {
-  return {
-    type: ADD,
-    text
-  };
-};
-
-const deleteToDo = id => {
-  return {
-    type: DELETE,
-    id
-  };
-};
-
-/*/
-// 리덕스 툴킷
 // const action = create(type)
 const addToDo = createAction('ADD');
 const deleteToDo = createAction('DELETE');
-/**/
 
-/**
-// 일반 리덕스
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter(toDo => toDo.id !== action.id);
-    default:
-      return state;
-  }
-};
-/*/
-// 리덕스 툴킷
 // payload는 action으로 전해진 인자값
 const reducer = (state = [], action) => {
   console.log(action);
@@ -53,7 +17,7 @@ const reducer = (state = [], action) => {
       return state;
   }
 };
-/**/
+
 const store = createStore(reducer);
 
 export const actionCreators = {
